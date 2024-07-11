6e938cd87b777db28c91e26039bcd5c6e67d85a8
Maciej Muchlinski <muchlinski@gmail.com>
2024-07-11 16:00:40 +0200

    python preprocessing package for NLP course

---
+++ README.md
@@ -0,0 +1,5 @@
+# Preprocessing Text Python Package
+
+Based on course 2024 NLP
+
+This python package is prepared by MM
---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('README.md', 'r') as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name='nlp_preprocess_mm',
+    version='0.0.1',
+    author='Maciej M',
+    description='This is preprocessing package',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3.12'
+)
